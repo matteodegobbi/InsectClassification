4b3eff83123052c5ee18de5ab468a094719fd3f3
matteodegobbi <matteodegobbi2001@gmail.com>
2024-05-23 22:20:40 +0200

    casino totale reacgan

---
+++ modelReACGAN.py
@@ -0,0 +1,431 @@
+import torch
+from torch import nn 
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+
+import os
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
+from dataset_utils import Fit_params, Save_samples_params,save_samples
+import torch.nn.functional as F
+
+
+import ops 
+from utils import misc
+class DiscOptBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, apply_d_sn, MODULES):
+        super(DiscOptBlock, self).__init__()
+        self.apply_d_sn = apply_d_sn
+
+        self.conv2d0 = MODULES.d_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv2d1 = MODULES.d_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2d2 = MODULES.d_conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+
+        if not apply_d_sn:
+            self.bn0 = MODULES.d_bn(in_features=in_channels)
+            self.bn1 = MODULES.d_bn(in_features=out_channels)
+
+        self.activation = MODULES.d_act_fn
+
+        self.average_pooling = nn.AvgPool2d(2)
+
+    def forward(self, x):
+        x0 = x
+        x = self.conv2d1(x)
+        if not self.apply_d_sn:
+            x = self.bn1(x)
+        x = self.activation(x)
+
+        x = self.conv2d2(x)
+        x = self.average_pooling(x)
+
+        x0 = self.average_pooling(x0)
+        if not self.apply_d_sn:
+            x0 = self.bn0(x0)
+        x0 = self.conv2d0(x0)
+        out = x + x0
+        return out
+
+
+class DiscBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, apply_d_sn, MODULES, downsample=True):
+        super(DiscBlock, self).__init__()
+        self.apply_d_sn = apply_d_sn
+        self.downsample = downsample
+
+        self.activation = MODULES.d_act_fn
+
+        self.ch_mismatch = False
+        if in_channels != out_channels:
+            self.ch_mismatch = True
+
+        if self.ch_mismatch or downsample:
+            self.conv2d0 = MODULES.d_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+            if not apply_d_sn:
+                self.bn0 = MODULES.d_bn(in_features=in_channels)
+
+        self.conv2d1 = MODULES.d_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2d2 = MODULES.d_conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+
+        if not apply_d_sn:
+            self.bn1 = MODULES.d_bn(in_features=in_channels)
+            self.bn2 = MODULES.d_bn(in_features=out_channels)
+
+        self.average_pooling = nn.AvgPool2d(2)
+
+    def forward(self, x):
+        x0 = x
+        if not self.apply_d_sn:
+            x = self.bn1(x)
+        x = self.activation(x)
+        x = self.conv2d1(x)
+
+        if not self.apply_d_sn:
+            x = self.bn2(x)
+        x = self.activation(x)
+        x = self.conv2d2(x)
+        if self.downsample:
+            x = self.average_pooling(x)
+
+        if self.downsample or self.ch_mismatch:
+            if not self.apply_d_sn:
+                x0 = self.bn0(x0)
+            x0 = self.conv2d0(x0)
+            if self.downsample:
+                x0 = self.average_pooling(x0)
+        out = x + x0
+        return out
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_size, d_conv_dim, apply_d_sn, apply_attn, attn_d_loc, d_cond_mtd, d_embed_dim, normalize_d_embed,
+                 num_classes, d_init, d_depth, mixed_precision, MODULES):
+        super(Discriminator, self).__init__()
+        d_in_dims_collection = {
+            "32": [3] + [d_conv_dim * 2, d_conv_dim * 2, d_conv_dim * 2],
+            "64": [3] + [d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8],
+            "128": [3] + [d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 16],
+            "256": [3] + [d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 8, d_conv_dim * 16],
+            "512": [3] + [d_conv_dim, d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 8, d_conv_dim * 16]
+        }
+
+        d_out_dims_collection = {
+            "32": [d_conv_dim * 2, d_conv_dim * 2, d_conv_dim * 2, d_conv_dim * 2],
+            "64": [d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 16],
+            "128": [d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 16, d_conv_dim * 16],
+            "256": [d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 8, d_conv_dim * 16, d_conv_dim * 16],
+            "512":
+            [d_conv_dim, d_conv_dim, d_conv_dim * 2, d_conv_dim * 4, d_conv_dim * 8, d_conv_dim * 8, d_conv_dim * 16, d_conv_dim * 16]
+        }
+
+        d_down = {
+            "32": [True, True, False, False],
+            "64": [True, True, True, True, False],
+            "128": [True, True, True, True, True, False],
+            "256": [True, True, True, True, True, True, False],
+            "512": [True, True, True, True, True, True, True, False]
+        }
+
+        self.d_cond_mtd = d_cond_mtd
+        self.normalize_d_embed = normalize_d_embed
+        self.num_classes = num_classes
+        self.mixed_precision = mixed_precision
+        self.in_dims = d_in_dims_collection[str(img_size)]
+        self.out_dims = d_out_dims_collection[str(img_size)]
+        down = d_down[str(img_size)]
+
+        self.blocks = []
+        for index in range(len(self.in_dims)):
+            if index == 0:
+                self.blocks += [[
+                    DiscOptBlock(in_channels=self.in_dims[index], out_channels=self.out_dims[index], apply_d_sn=apply_d_sn, MODULES=MODULES)
+                ]]
+            else:
+                self.blocks += [[
+                    DiscBlock(in_channels=self.in_dims[index],
+                              out_channels=self.out_dims[index],
+                              apply_d_sn=apply_d_sn,
+                              MODULES=MODULES,
+                              downsample=down[index])
+                ]]
+
+            if index + 1 in attn_d_loc and apply_attn:
+                self.blocks += [[ops.SelfAttention(self.out_dims[index], is_generator=False, MODULES=MODULES)]]
+
+        self.blocks = nn.ModuleList([nn.ModuleList(block) for block in self.blocks])
+
+        self.activation = MODULES.d_act_fn
+
+        # linear layer for adversarial training
+        
+        self.linear1 = MODULES.d_linear(in_features=self.out_dims[-1], out_features=1, bias=True)
+
+
+
+        self.linear2 = MODULES.d_linear(in_features=self.out_dims[-1], out_features=d_embed_dim, bias=True)
+        self.embedding = MODULES.d_embedding(num_classes, d_embed_dim)
+        if d_init:
+            ops.init_weights(self.modules, d_init)
+
+    def forward(self, x, label, eval=False, adc_fake=False):
+        with torch.cuda.amp.autocast() if self.mixed_precision and not eval else misc.dummy_context_mgr() as mp:
+            embed, proxy, cls_output = None, None, None
+            h = x
+            for index, blocklist in enumerate(self.blocks):
+                for block in blocklist:
+                    h = block(h)
+            bottom_h, bottom_w = h.shape[2], h.shape[3]
+            h = self.activation(h)
+            h = torch.sum(h, dim=[2, 3])
+
+            # adversarial training
+            adv_output = torch.squeeze(self.linear1(h))
+
+            embed = self.linear2(h)
+            proxy = self.embedding(label)
+            if self.normalize_d_embed:
+                embed = F.normalize(embed, dim=1)
+                proxy = F.normalize(proxy, dim=1)
+        return {
+            "h": h,
+            "adv_output": adv_output,
+            "embed": embed,
+            "proxy": proxy,
+            "cls_output": cls_output,
+            "label": label,
+        }
+
+
+class GenBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, g_cond_mtd, affine_input_dim, MODULES):
+        super(GenBlock, self).__init__()
+        self.g_cond_mtd = g_cond_mtd
+        self.bn1 = MODULES.g_bn(affine_input_dim, in_channels, MODULES)
+        self.bn2 = MODULES.g_bn(affine_input_dim, out_channels, MODULES)
+        self.activation = MODULES.g_act_fn
+        self.conv2d0 = MODULES.g_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv2d1 = MODULES.g_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2d2 = MODULES.g_conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x, affine):
+        x0 = x
+        x = self.bn1(x, affine)
+        x = self.activation(x)
+        x = F.interpolate(x, scale_factor=2, mode="nearest")
+        x = self.conv2d1(x)
+        x = self.bn2(x, affine)
+        x = self.activation(x)
+        x = self.conv2d2(x)
+        x0 = F.interpolate(x0, scale_factor=2, mode="nearest")
+        x0 = self.conv2d0(x0)
+        out = x + x0
+        return out
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, g_shared_dim, img_size, g_conv_dim, apply_attn, attn_g_loc, g_cond_mtd, num_classes, g_init, g_depth,
+                 mixed_precision, MODULES ):
+        super(Generator, self).__init__()
+        g_in_dims_collection = {
+            "32": [g_conv_dim * 4, g_conv_dim * 4, g_conv_dim * 4],
+            "64": [g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2],
+            "128": [g_conv_dim * 16, g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2],
+            "256": [g_conv_dim * 16, g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2],
+            "512": [g_conv_dim * 16, g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2, g_conv_dim]
+        }
+
+        g_out_dims_collection = {
+            "32": [g_conv_dim * 4, g_conv_dim * 4, g_conv_dim * 4],
+            "64": [g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2, g_conv_dim],
+            "128": [g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2, g_conv_dim],
+            "256": [g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2, g_conv_dim],
+            "512": [g_conv_dim * 16, g_conv_dim * 8, g_conv_dim * 8, g_conv_dim * 4, g_conv_dim * 2, g_conv_dim, g_conv_dim]
+        }
+
+        bottom_collection = {"32": 4, "64": 4, "128": 4, "256": 4, "512": 4}
+
+        self.z_dim = z_dim
+        self.num_classes = num_classes
+        self.g_cond_mtd = g_cond_mtd
+        self.mixed_precision = mixed_precision
+        self.in_dims = g_in_dims_collection[str(img_size)]
+        self.out_dims = g_out_dims_collection[str(img_size)]
+        self.bottom = bottom_collection[str(img_size)]
+        self.num_blocks = len(self.in_dims)
+        self.affine_input_dim = 0
+
+ 
+
+        self.linear0 = MODULES.g_linear(in_features=self.z_dim, out_features=self.in_dims[0] * self.bottom * self.bottom, bias=True)
+
+        self.affine_input_dim += self.num_classes
+
+        self.blocks = []
+        for index in range(self.num_blocks):
+            self.blocks += [[
+                GenBlock(in_channels=self.in_dims[index],
+                         out_channels=self.out_dims[index],
+                         g_cond_mtd=self.g_cond_mtd,
+                         affine_input_dim=self.affine_input_dim,
+                         MODULES=MODULES)
+            ]]
+
+            if index + 1 in attn_g_loc and apply_attn:
+                self.blocks += [[ops.SelfAttention(self.out_dims[index], is_generator=True, MODULES=MODULES)]]
+
+        self.blocks = nn.ModuleList([nn.ModuleList(block) for block in self.blocks])
+
+        self.bn4 = ops.batchnorm_2d(in_features=self.out_dims[-1])
+        self.activation = MODULES.g_act_fn
+        self.conv2d5 = MODULES.g_conv2d(in_channels=self.out_dims[-1], out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.tanh = nn.Tanh()
+
+        ops.init_weights(self.modules, g_init)
+
+    def forward(self, z, label, shared_label=None, eval=False):
+        affine_list = []
+        label = F.one_hot(label, num_classes=self.num_classes).to(torch.float32)
+        with torch.cuda.amp.autocast() if self.mixed_precision and not eval else misc.dummy_context_mgr() as mp:
+            affine_list.append(label)
+            if len(affine_list) > 0:
+                affines = torch.cat(affine_list, 1)
+            else:
+                affines = None
+
+            act = self.linear0(z)
+            act = act.view(-1, self.in_dims[0], self.bottom, self.bottom)
+            for index, blocklist in enumerate(self.blocks):
+                for block in blocklist:
+                    if isinstance(block, ops.SelfAttention):
+                        act = block(act)
+                    else:
+                        act = block(act, affines)
+
+            act = self.bn4(act)
+            act = self.activation(act)
+            act = self.conv2d5(act)
+            out = self.tanh(act)
+        return out
+#####################
+#####################
+#####################
+#####################
+
+from tqdm.notebook import tqdm
+
+def train_discriminator(real_images,real_classes , discriminator_optimizer,discriminator,generator,batch_size,latent_size,described_species,device,n_classes):
+    # Clear discriminator gradients
+    discriminator_optimizer.zero_grad()
+
+    # Pass real images through discriminator
+    real_preds,real_predicted_classes = discriminator(real_images)
+    real_preds_targets = torch.ones(real_images.size(0), 1, device=device)
+
+    #print(real_classes)
+    #print(real_predicted_classes)
+    '''loss_pred_real + loss_class_real +
+    loss_pred_fake + loss_class_fake'''
+  
+    real_loss = nn.functional.binary_cross_entropy(real_preds, real_preds_targets) + nn.functional.nll_loss(real_predicted_classes,real_classes)
+    real_score = torch.mean(real_preds).item()
+    
+    # Generate fake images
+    latent = torch.randn(batch_size, latent_size, 1, 1, device=device)
+    #random_classes = torch.tensor(np.random.randint(0, n_classes, batch_size),device=device)
+    random_classes = torch.tensor(described_species[np.random.randint(0, len(described_species), batch_size)],device=device)
+    fake_images = generator(latent,random_classes)
+
+    #print(fake_images.shape)
+    # Pass fake images through discriminator
+    fake_preds_targets = torch.zeros(fake_images.size(0),1, device=device)
+    fake_preds,fake_predicted_classes = discriminator(fake_images)
+    fake_loss = nn.functional.binary_cross_entropy(fake_preds, fake_preds_targets) + nn.functional.nll_loss(fake_predicted_classes,random_classes)
+    fake_score = torch.mean(fake_preds).item()
+
+    # Update discriminator weights
+    loss = real_loss + fake_loss
+    loss.backward()
+    discriminator_optimizer.step()
+
+    class_accuracy_real = 0
+    most_likely_classes = torch.argmax(real_predicted_classes, 1)
+    for index in range(len(real_classes)):
+        if most_likely_classes[index] == real_classes[index]:
+            class_accuracy_real += 1
+    class_accuracy_real /= batch_size
+        
+    class_accuracy_fake = 0
+    most_likely_classes = torch.argmax(fake_predicted_classes, 1)
+    for index in range(len(random_classes)):
+        if most_likely_classes[index] == random_classes[index]:
+            class_accuracy_fake += 1
+    class_accuracy_fake /= batch_size
+    
+    return loss.item(), real_score, fake_score, class_accuracy_real, class_accuracy_fake
+    
+def train_generator(generator_optimizer,generator,discriminator,batch_size,latent_size,described_species,device,n_classes):
+    # Clear generator gradients
+    generator_optimizer.zero_grad()
+    
+    # Generate fake images
+    latent = torch.randn(batch_size, latent_size, 1, 1,device=device)
+    #random_classes = torch.tensor(np.random.randint(0, n_classes, batch_size),device=device)
+    random_classes = torch.tensor(described_species[np.random.randint(0, len(described_species), batch_size)],device=device)
+
+    
+    fake_images = generator(latent,random_classes)
+    # Try to fool the discriminator
+    preds,predicted_classes = discriminator(fake_images)
+    preds_targets = torch.ones(batch_size, 1, device=device)
+    
+    loss_preds = nn.functional.binary_cross_entropy(preds, preds_targets)
+    
+    # target must not be one-hot encoded while predicted classes are batch_size x number_of_classes log probabilities
+    loss_class = nn.functional.nll_loss(predicted_classes,random_classes)
+    
+    
+    loss =  loss_preds + loss_class 
+    # Update generator weights
+    loss.backward()
+    generator_optimizer.step()
+    
+    return loss.item()
+def fit(epochs,fit_p : Fit_params, save_p : Save_samples_params,start_idx=0,latent_size =100):
+    torch.cuda.empty_cache()
+    
+    # Losses & scores
+    losses_g = []
+    losses_d = []
+    real_scores = []
+    fake_scores = []
+    
+    
+    for epoch in range(epochs):
+        for real_images, real_classes in tqdm(fit_p.dataloaders['train']):
+            # Train discriminator
+            real_images = real_images.to(fit_p.device)
+            real_classes = real_classes.to(fit_p.device)
+            loss_d, real_score, fake_score, class_accuracy_real, class_accuracy_fake = train_discriminator(real_images, real_classes,fit_p.discriminator_optimizer,fit_p.discriminator,fit_p.generator,fit_p.batch_size,fit_p.latent_size,fit_p.described_species_labels,fit_p.device,fit_p.n_classes)
+            # Train generator FOR MORE TIMES THAN DISCRIMINATOR
+            #for k in range(3):
+            loss_g = train_generator(fit_p.generator_optimizer,fit_p.generator,fit_p.discriminator,fit_p.batch_size,fit_p.latent_size,fit_p.described_species_labels,fit_p.device,fit_p.n_classes)
+            
+        # Record losses & scores
+        losses_g.append(loss_g)
+        losses_d.append(loss_d)
+        real_scores.append(real_score)
+        fake_scores.append(fake_score)
+        fit_p.writer.add_scalar('loss_g', loss_g, epoch)
+        fit_p.writer.add_scalar('loss_d', loss_d, epoch)
+        fit_p.writer.add_scalar('class_accuracy_real', class_accuracy_real, epoch)
+        fit_p.writer.add_scalar('class_accuracy_fake', class_accuracy_fake, epoch)
+        # Log losses & scores (last batch)
+        print("Epoch [{}/{}], loss_g: {:.4f}, loss_d: {:.4f}, real_score: {:.4f}, fake_score: {:.4f}".format(
+            epoch+1, epochs, loss_g, loss_d, real_score, fake_score))
+        print(f"class accuracy real {class_accuracy_real}")
+        print(f"class accuracy fake {class_accuracy_fake}")
+        # Save generated images
+        save_samples(epoch+start_idx, save_p,fit_p.generator,fit_p.writer,show=False)
+    
+    return losses_g, losses_d, real_scores, fake_scores
+
