bbc227e230369da299b9728368c29c986fbc211c
matteodegobbi <matteodegobbi2001@gmail.com>
2024-10-17 20:52:38 +0200

    codice download new samples lento

--- get_unseen_fresh_samples/sample_5_per_cent.py
+++ get_unseen_fresh_samples/sample_5_per_cent.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 df = pd.read_csv('df_unknown_species.csv', delimiter='\t', encoding='latin')
 
-df = df.dropna(subset=['image_urls'])
+df = df.dropna(subset=['image_urls']) #remove dnas with no corresponding image
 
-sample_fraction = 0.10
-
-sampled_df = df.groupby('genus_name').apply(lambda x: x.sample(frac=sample_fraction if len(x) > 1 else len(x))).reset_index(drop=True)
+# if there are more than 1000 samples pick only 1000
+n_samples = 1000
+sampled_df = df.groupby('genus_name').apply(lambda x: x.sample(frac=n_samples/len(x) if len(x) > n_samples else 1.0)).reset_index(drop=True)
 
 sampled_df.to_csv('unknown_species_new_samples.csv', index=False)
 
